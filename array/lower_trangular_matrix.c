c4aed665fa8a4388e8206715cdb8ec6fbabb8c9b
Arvind kumar <arvindkumar628736@gmail.com>
2026-08-14 22:51:35 +0530

    Add lower trangular matrix array progaram

---
+++ array/lower_trangular_matrix.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+  int arr[3][3];
+  printf("enter your matrix element 3*3:");
+  for(int i=0;i<3;i++){
+    for(int j=0;j<3;j++){
+      scanf("%d",&arr[i][j]);
+    }
+  }
+  printf("lower tranguler matrix is \n");
+  for(int i=0;i<3;i++){
+    for(int j=0;j<3;j++){
+      if (i==j||i>j){
+        printf(" %d",arr[i][j]);
+      }else{
+        printf(" 0");
+      }
+
+    }
+    printf("\n");
+  }
+  return 0;
+}
